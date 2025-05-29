7219d7c535326baaace47ba627198b22ee2daa13
Jaedyn <61067221+Jcarbonell04@users.noreply.github.com>
2025-05-28 23:18:35 -0600

    Added GDIDrawer

---
+++ tetris-gdi-drawer/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GDIDrawer;
+
+namespace tetris_gdi_drawer
+{
+    public partial class Form1 : Form
+    {
+        private const int canvasHeight = 600;
+        private const int canvasWidth = 800;
+        private CDrawer canvas; 
+
+        public Form1()
+        {
+            InitializeComponent();
+            canvas = new CDrawer(canvasWidth, canvasHeight);
+        }
+    }
+}
