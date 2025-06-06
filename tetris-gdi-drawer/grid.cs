81e3565fcf6245bfcafb9207e95be50d73020669
Jaedyn <61067221+Jcarbonell04@users.noreply.github.com>
2025-06-05 23:53:32 -0600

    Update grid.cs

--- tetris-gdi-drawer/grid.cs
+++ tetris-gdi-drawer/grid.cs
@@ -13,15 +13,47 @@ namespace tetris_gdi_drawer
         private Color[] colors;
 
         public int numRows { get; set; } = 20;
-        public int numCols { get;  set; } = 10;
+        public int numCols { get; set; } = 10;
         public int cellSize { get; set; } = 30;
+        public Color cellColor { get; set; }
 
-        public grid(int rows, int cols)
+        public grid()
         {
-            numRows = rows;
-            numCols = cols;
-            gameGrid = new int[rows, cols];
+            numRows = 20;
+            numCols = 10;
+            cellSize = 30;
+            gameGrid = new int[numRows, numCols];
+            cellColor = Color.White; // chnage later , no definition for getcellcolors yet
+        }
 
+        bool IsInside(int row, int col)
+        {
+            if (row >= 0 && row < numRows && col >= 0 && col < numCols)
+                return true;
+            return false;
+        }
+
+        bool IsEmpty(int row, int col)
+        {
+            if (gameGrid[row, col] == 9)
+                return true;
+            return false;
+        }
+
+        bool IsRowFull(int row)
+        {
+            for (int col = 0; col < numCols; col++)
+                if (gameGrid[row, col] == 0)
+                    return false;
+            return true;
+        }
+
+        void ClearRow(int row)
+        {
+            for (int col = 0; col < numCols; col++)
+            {
+                gameGrid[row, col] = 0;
+            }
         }
     }
 }
