8e2d4e1fbb5a4d07501a1e652d4732e1f7c1dddd
Jaedyn <61067221+Jcarbonell04@users.noreply.github.com>
2025-06-09 20:40:15 -0600

    Constructor created

--- tetris-gdi-drawer/game.cs
+++ tetris-gdi-drawer/game.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace tetris_gdi_drawer
 {
-    internal class game
+    public class game
     {
+        public game()
+        {
+            grid grid = new grid();
+            List<Block> blocks = new List<Block> 
+            {
+                new iBlock(),
+                new jBlock(),
+                new lBlock(),
+                new oBlock(),
+                new sBlock(),
+                new tBlock(),
+                new zBlock()
+            };
+            Block currentBlock; // get random block
+            Block nextBlock; // get random block
+            bool gameOver = false;
+            int score = 0;
+        }
     }
 }
